9a1936690904413b6acd8865da48a8213940ce7c
Santiago Loza <lozasantiago8@gmail.com>
2022-08-01 13:56:03 -0300

    Arreglo en la busqueda de la palabra mas corta

--- scripts/statsWords.sh
+++ scripts/statsWords.sh
@@ -27,27 +27,34 @@ function compara_longitud {
 
 #funcion que recibe un archivo de texto. Hace uso de la funcion compara_longitud definida anteriormente, e imprime la palabra mas larga, mas corta y el promedio de longitud.
 function principal {
-	#Utilizaremos las variables SUMA Y CANT_PALABRAS para calcular el promedio de longitud
+	#Utilizaremos las variables SUMA Y CANT_PALABRAS para calcular el promedio de longitud.
 	SUMA=0
 	CANT_PALABRAS=0
 
 	for PALABRA in $(cat $1)
 	do
+		#contamos la cantidad de palabras que vamos recorriendo.
+		((CANT_PALABRAS ++))
+
 		if ( compara_longitud $PALABRA $PALABRA_MAS_LARGA )
 		then
 			PALABRA_MAS_LARGA=$PALABRA
 		fi
 
-		if ( compara_longitud $PALABRA_MAS_CORTA $PALABRA )
+		#asignamos la primera palabra como la mas corta para poder comparar con las siguientes.
+		if [ $CANT_PALABRAS -eq 1 ]
+		then
+			PALABRA_MAS_CORTA=$PALABRA
+		fi
+
+		#ademas de verificar si la palabra es mas corta que la establecida como mas corta, verificamos si tambien es mayor a 1 letra.
+		if  (compara_longitud $PALABRA_MAS_CORTA $PALABRA ) && [ ${#PALABRA} -gt 1 ] 
 		then
 			PALABRA_MAS_CORTA=$PALABRA
 		fi
 
 		#sumamos todas las longitudes de las palabras
 		SUMA=$(($SUMA + ${#PALABRA}))
-
-		#contamos la cantidad de palabras 
-		((CANT_PALABRAS++))
 					
 	done
 
