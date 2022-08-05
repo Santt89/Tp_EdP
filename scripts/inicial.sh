ca30e5c588b3ead1a7c8323900c1ae4bd83f2e09
Santiago Loza <91545978+Santt89@users.noreply.github.com>
2022-08-05 15:38:18 -0300

    agregado de opcion salir

--- scripts/inicial.sh
+++ scripts/inicial.sh
@@ -5,29 +5,32 @@ PS3="Introduzca la opcion que desea utilizar: "
 
 echo "Scripts para filtrado: "
 
-select opcion in "statsWords.sh" "statsUsageWords.sh" "findNames.sh" "statsSentences.sh" "blankLinesCounter.sh"
+select opcion in "statsWords.sh" "statsUsageWords.sh" "findNames.sh" "statsSentences.sh" "blankLinesCounter.sh" "salir"
 do
         case $opcion in
         "statsWords.sh")
                 bash /root/scripts/statsWords.sh /root/scripts/texto
-                break
+            
                 ;;
         "statsUsageWords.sh")
                 bash /root/scripts/statsUsageWords.sh /root/scripts/texto
-                break
+               
                 ;;
         "findNames.sh")
                 bash /root/scripts/findNames.sh /root/scripts/texto
-                break
+                
                 ;;
         "statsSentences.sh")
                 bash /root/scripts/statsSentences.sh /root/scripts/texto
-                break
+                
                 ;;
 	"blankLinesCounter.sh")
                 bash /root/scripts/blankLinesCounter.sh /root/scripts/texto
-                break
+                
                 ;;
+	"salir")
+		break
+		;;
 
         esac
 done
